51647df2e5bfd63312fd2375aefe9bd461a2ec00
jdyo <719999727@qq.com>
2020-05-18 08:47:47 +0800

    Update a_pod-ex-minimum.py

--- a_pod-ex-minimum.py
+++ a_pod-ex-minimum.py
@@ -20,8 +20,6 @@ kubernetes_min_pod = kubernetes_pod_operator.KubernetesPodOperator(
         # entrypoint is used. The cmds parameter is templated.
         cmds=['echo'],
         config_file="/usr/local/airflow/config",
-        volumes=[volume],
-        volume_mounts=[volume_mount],
         # The namespace to run within Kubernetes, default namespace is
         # `default`. There is the potential for the resource starvation of
         # Airflow workers and scheduler within the Cloud Composer environment,
