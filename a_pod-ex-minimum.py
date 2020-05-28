a1786427e2a92a0f7214585b9b33372fc903150a
jdyo <719999727@qq.com>
2020-05-28 09:42:51 +0800

    Update a_pod-ex-minimum.py

--- a_pod-ex-minimum.py
+++ a_pod-ex-minimum.py
@@ -9,26 +9,10 @@ dag=models.DAG(
             start_date=YESTERDAY) 
         
 kubernetes_min_pod = kubernetes_pod_operator.KubernetesPodOperator(
-        # The ID specified for the task.
         task_id='pod-ex-minimum',
-        # Name of task you want to run, used to generate Pod ID.
         dag=dag,
         name='pod-ex-minimum',
-        # Entrypoint of the container, if not specified the Docker container's
-        # entrypoint is used. The cmds parameter is templated.
         cmds=['echo','hello'],
-        config_file="/var/airflow/secrets/kubeconfig/kube.config",
-        # The namespace to run within Kubernetes, default namespace is
-        # `default`. There is the potential for the resource starvation of
-        # Airflow workers and scheduler within the Cloud Composer environment,
-        # the recommended solution is to increase the amount of nodes in order
-        # to satisfy the computing requirements. Alternatively, launching pods
-        # into a custom namespace will stop fighting over resources.
+        #config_file="/var/airflow/secrets/kubeconfig/kube.config",
         namespace='airflow',
-        # Docker image specified. Defaults to hub.docker.com, but any fully
-        # qualified URLs will point to a custom repository. Supports private
-        # gcr.io images if the Composer Environment is under the same
-        # project-id as the gcr.io images and the service account that Composer
-        # uses has permission to access the Google Container Registry
-        # (the default service account has permission)
         image='ubuntu:16.04')
