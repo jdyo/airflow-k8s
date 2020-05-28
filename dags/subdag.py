8162d5f31466a1026b3ca545b181d1a792047770
jdyo <719999727@qq.com>
2020-05-28 15:18:21 +0800

    Update subdag.py

--- dags/subdag.py
+++ dags/subdag.py
@@ -1,1 +1,18 @@
-#
+import datetime
+from airflow import models
+from airflow.contrib.operators import kubernetes_pod_operator
+
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+dag=models.DAG(
+            dag_id='a_pod-ex-minimum',
+            schedule_interval=datetime.timedelta(days=1),
+            start_date=YESTERDAY) 
+        
+kubernetes_min_pod = kubernetes_pod_operator.KubernetesPodOperator(
+        task_id='pod-ex-minimum',
+        dag=dag,
+        name='pod-ex-minimum',
+        cmds=['echo','hello'],
+        #config_file="/var/airflow/secrets/kubeconfig/kube.config",
+        namespace='airflow',
+        image='ubuntu:16.04')
